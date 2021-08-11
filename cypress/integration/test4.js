3453e35017e216a81edbca2fcc3b9995d8107e28
Daniela <dhiguitaa17@gmail.com>
2021-08-11 07:37:04 -0500

    iframes

--- cypress/integration/test4.js
+++ cypress/integration/test4.js
@@ -1,64 +1,13 @@
 /// <reference types='Cypress'/>
-describe('Multiple elements test suite', () => {
-  it('case', () => {
+/// <reference types='cypress-iframe'/>
+import 'cypress-iframe';
+
+describe('Frames test suite', () => {
+  it('frame case', () => {
     cy.visit('https://rahulshettyacademy.com/AutomationPractice');
-
-    //check boxes
-    cy.get('#checkBoxOption1')
-      .check()
-      .should('be.checked')
-      .and('have.value', 'option1');
-    cy.get('#checkBoxOption1').uncheck().should('be.not.checked');
-    cy.get('input[type="checkbox"]').check(['option2', 'option3']);
-
-    //static dropdown
-    cy.get('select').select('option2').should('have.value', 'option2');
-
-    //dinamic dropdown
-    cy.get('#autocomplete').type('ind');
-    cy.get('.ui-menu-item').each(($item, index, $list) => {
-      if ($item.text() === 'India') {
-        $item.click();
-      }
-    });
-    cy.get('#autocomplete').should('have.value', 'India');
-
-    //visibility
-    cy.get('#displayed-text').should('be.visible');
-    cy.get('#hide-textbox').click();
-    cy.get('#displayed-text').should('not.be.visible');
-    cy.get('#show-textbox').click();
-
-    //pop-up or alerts
-    cy.get('#alertbtn').click();
-    cy.get('#confirmbtn').click();
-    cy.on('window:alert', (alertText) => {
-      expect(alertText).to.be.equal(
-        'Hello , share this practice page and share your knowledge'
-      );
-    });
-    //window open in a new tab
-    cy.get('#opentab').invoke('removeAttr', 'target').click();
-    cy.url().should('include', 'rahulshettyacademy');
-    cy.go('back');
-
-    //siblings
-    cy.get('tr td:nth-child(2)').each(($item, index, $list) => {
-      const text = $item.text();
-      if (text.includes('Python')) {
-        cy.get('tr td:nth-child(2)')
-          .eq(index)
-          .next()
-          .then((price) => {
-            const priceText = price.text();
-            expect(priceText).to.equal('25');
-          });
-      }
-    });
-
-    //mouse over
-    cy.get('.mouse-hover-content').invoke('show');
-    cy.contains('Top').click();
-    cy.url().should('include', 'top');
+    cy.frameLoaded('#courses-iframe'); //which is the iframe
+    //swich to the iframe
+    cy.iframe().find('a[href*="mentorship"]').eq(0).click();
+    cy.iframe().find('h1[class*="pricing-title"]').should('have.length', 2);
   });
 });
