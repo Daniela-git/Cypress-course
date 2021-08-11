aac248f932e86be76181f7b05b751979f9ac9877
Daniela <dhiguitaa17@gmail.com>
2021-08-11 06:39:01 -0500

    test 2 finished and test 3 going

--- cypress/integration/test2.js
+++ cypress/integration/test2.js
@@ -1,17 +1,10 @@
 /// <reference types='Cypress'/>
-describe('My first test suite', () => {
-  it('my first case', () => {
+describe('My second test suite', () => {
+  it('my second case', () => {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
     cy.get('.search-keyword').type('ca');
     cy.wait(2000);
-    cy.get('.product:visible').should('have.length', 4);
     cy.get('.products').as('productsLocator');
-    cy.get('@productsLocator').find('.product').should('have.length', 4);
-    cy.get('@productsLocator')
-      .find('.product')
-      .eq(1)
-      .contains('ADD TO CART')
-      .click();
     cy.get('@productsLocator')
       .find('.product')
       .each(($item, index, $list) => {
@@ -20,5 +13,8 @@ describe('My first test suite', () => {
           $item.find('button').click();
         }
       });
+    cy.get('.cart-icon > img').click();
+    cy.contains('PROCEED TO CHECKOUT').click();
+    cy.contains('Place Order').click();
   });
 });
---
+++ cypress/integration/test3.js
@@ -0,0 +1,59 @@
+/// <reference types='Cypress'/>
+describe('Multiple elements test suite', () => {
+  it('case', () => {
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice');
+
+    //check boxes
+    cy.get('#checkBoxOption1')
+      .check()
+      .should('be.checked')
+      .and('have.value', 'option1');
+    cy.get('#checkBoxOption1').uncheck().should('be.not.checked');
+    cy.get('input[type="checkbox"]').check(['option2', 'option3']);
+
+    //static dropdown
+    cy.get('select').select('option2').should('have.value', 'option2');
+
+    //dinamic dropdown
+    cy.get('#autocomplete').type('ind');
+    cy.get('.ui-menu-item').each(($item, index, $list) => {
+      if ($item.text() === 'India') {
+        $item.click();
+      }
+    });
+    cy.get('#autocomplete').should('have.value', 'India');
+
+    //visibility
+    cy.get('#displayed-text').should('be.visible');
+    cy.get('#hide-textbox').click();
+    cy.get('#displayed-text').should('not.be.visible');
+    cy.get('#show-textbox').click();
+
+    //pop-up or alerts
+    cy.get('#alertbtn').click();
+    cy.get('#confirmbtn').click();
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.be.equal(
+        'Hello , share this practice page and share your knowledge'
+      );
+    });
+    //window open in a new tab
+    cy.get('#opentab').invoke('removeAttr', 'target').click();
+    cy.url().should('include', 'rahulshettyacademy');
+    cy.go('back');
+
+    //siblings
+    cy.get('tr td:nth-child(2)').each(($item, index, $list) => {
+      const text = $item.text();
+      if (text.includes('Python')) {
+        cy.get('tr td:nth-child(2)')
+          .eq(index)
+          .next()
+          .then((price) => {
+            const priceText = price.text();
+            expect(priceText).to.equal('25');
+          });
+      }
+    });
+  });
+});
